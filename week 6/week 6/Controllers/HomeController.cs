9aeb3e26ab07b6f6b008db61aaef89415aa89a61
Furkan Gözükara <furkangozukara@gmail.com>
2020-03-04 11:02:16 +0300

    week 6 v 5

---
+++ week 6/week 6/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using week_6.Models;
+
+namespace week_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Home")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Index(csWebUser myModel)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ week 6/week 6/Models/csWebUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace week_6.Models
+{
+    public class csWebUser
+    {
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string MailAddress { get; set; }
+    }
+}
